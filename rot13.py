753a45a9e630fdfcb31259655e36a28671c554e1
N3d <matteo.casenove@gmail.com>
2013-05-16 13:58:26 +0200

    add cesar13 crypting algorithm

---
+++ rot13.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+        if len(argv) <= 1:
+            print("rot13: require the string to encode\n")
+            sys.exit()
+    original = argv[1];
+    encode = original.encode("rot13")
+    print("Original: {0} \nCrypted: {1} \n".format(original,encode))
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
